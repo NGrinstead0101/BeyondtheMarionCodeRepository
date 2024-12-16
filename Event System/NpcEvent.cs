ecc2637c649f19aa84161f7037617086f944a475
NGrinstead0101 <ngrinstead01@gmail.com>
2024-12-16 10:49:26 -0600

    Added Event System Scripts

---
+++ Event System/NpcEvent.cs
@@ -0,0 +1,62 @@
+/******************************************************************
+*    Author: Nick Grinstead
+*    Contributors: 
+*    Date Created: 5/16/24
+*    Description: A template event script to create ScriptableObject events
+*       from. Tracks listeners and updates them when the event triggers.
+*    Reference/Source: https://youtu.be/J01z1F-du-E?t=635 (10:32 - 12:45)
+*******************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enum for event tags used in differentiating events of the same type
+/// </summary>
+public enum NpcEventTags
+{
+    Angel,
+    Coward,
+    Fish,
+    Game,
+    Goop,
+    Robot,
+
+    GeneratorDeath,
+    FireDeath,
+    ShipDeath
+}
+
+[CreateAssetMenu(menuName = "NPC Event")]
+public class NpcEvent : ScriptableObject
+{
+    private List<NpcEventListener> _eventListeners = new List<NpcEventListener>();
+
+    /// <summary>
+    /// Called from another script to trigger this event. Event tags serve to 
+    /// distinguish events related to different NPCs, quests, items, etc.
+    /// </summary>
+    public void TriggerEvent(NpcEventTags eventTag)
+    {
+        for (int i = 0; i < _eventListeners.Count; ++i)
+        {
+            _eventListeners[i].OnEventTriggered(eventTag, this);
+        }
+    }
+
+    /// <summary>
+    /// Allows NpcEventListeners to register themselves.
+    /// </summary>
+    public void AddListener(NpcEventListener newListener)
+    {
+        _eventListeners.Add(newListener);
+    }
+
+    /// <summary>
+    /// Allows NpcEventListeners to unregister themselves
+    /// </summary>
+    public void RemoveListener(NpcEventListener listenerToRemove)
+    {
+        _eventListeners.Remove(listenerToRemove);
+    }
+}
---
+++ Event System/NpcEventListener.cs
@@ -0,0 +1,75 @@
+/******************************************************************
+*    Author: Nick Grinstead
+*    Contributors: 
+*    Date Created: 5/16/24
+*    Description: Add this script to any object that needs to listen
+*       for certain events to occur. Will invoke methods on various
+*       components when event is recieved.
+*    Reference/Source: https://youtu.be/J01z1F-du-E?t=635 (10:32 - 12:45)
+*******************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NpcEventListener : MonoBehaviour
+{
+    /// <summary>
+    /// This struct contains an event to listen for and a corresponding UnityEvent
+    /// to invoke when that event is triggered
+    /// </summary>
+    [System.Serializable]
+    private struct TargetEvent
+    {
+        [SerializeField] private NpcEvent _npcEvent;
+        [SerializeField] private NpcEventTags _npcEventTag;
+        [SerializeField] private UnityEvent _onEventTriggered;
+
+        public NpcEvent NpcEvent { get => _npcEvent; }
+        public NpcEventTags NpcEventTag { get => _npcEventTag; }
+
+        public void InvokeUnityEvents()
+        {
+            _onEventTriggered.Invoke();
+        }
+    }
+
+    [SerializeField] private TargetEvent[] _eventsToListenFor; 
+
+    /// <summary>
+    /// Called by NpcEvent to invoke a local UnityEvent if the incoming event tag
+    /// matches to target.
+    /// </summary>
+    public void OnEventTriggered(NpcEventTags eventTag, NpcEvent triggeredEvent)
+    {
+        foreach (TargetEvent targetEvent in _eventsToListenFor)
+        {
+            if (targetEvent.NpcEventTag == eventTag && targetEvent.NpcEvent == triggeredEvent)
+            {
+                targetEvent.InvokeUnityEvents();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Subscribes to all events in list
+    /// </summary>
+    private void OnEnable()
+    {
+        foreach (TargetEvent targetEvent in _eventsToListenFor)
+        {
+            targetEvent.NpcEvent.AddListener(this);
+        }
+    }
+
+    /// <summary>
+    /// Unsubscribes from all events in list
+    /// </summary>
+    private void OnDisable()
+    {
+        foreach (TargetEvent targetEvent in _eventsToListenFor)
+        {
+            targetEvent.NpcEvent.RemoveListener(this);
+        }
+    }
+}
